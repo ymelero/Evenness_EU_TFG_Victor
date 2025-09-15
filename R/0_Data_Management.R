4b545a7658e8e8bcf149330ffc01c5e5b60827a7
Yolanda Melero <y.melero@creaf.uab.cat>
2025-09-15 17:22:43 +0200

    # code

--- R/0_Data_Management.R
+++ R/0_Data_Management.R
@@ -1,6 +1,6 @@
 # -------------------------------------------------------------------------
 # SCRIPT TO PREPARE THE DATA: S-index (avaiable) - missing species counts / site / year
-# Author: [Papallones Urbanitas Team: YM]
+# Author: [Papallones Urbanitas Team: YM, DGC]
 # Inputs:
 #   - eBMS counts
 #   - eBMS s-index
@@ -19,11 +19,35 @@ library(tidyverse)
 
 ## 2.1 eBMS s-index data. per species, site, year. Calculated following Colom et al 2025
 # Give the path to the data, load them all and create an unique DB. 
-path <- "/Volumes/GoogleDrive/My Drive/DATA/s-indexes_ebms_Until2023" #YM computer
+path <- "/Users/SUITCASE/Downloads/sindex_results" #YM computer
 
 # bind all files
 list.files(path, pattern="\\.csv$", full.names=TRUE) 
-Db <- rbindlist(lapply(list.files(path, pattern="\\.csv$", full.names=TRUE), fread))
-write.table(Db, "Data/Db_sindex_all.txt", row.names = F)
+sindex_eu <- rbindlist(lapply(list.files(path, pattern="\\.csv$", full.names=TRUE), fread))
+write.table(sindex_eu, "Data/Db_sindex_all.txt", row.names = F, sep=";")
+# once done you can directly read the DB from your local folder:
+sindex_eu <- read.delim("Data/Db_sindex_all.txt", sep=";")
+
+## 2.2 eBMS raw count data. To merge with s-index and detect those that failed
+count <- read.csv2("Data/ebms_count.csv", sep=",")
+
+## 2.3 merge (unir) las DBs filling those that were count but there is no info on s-index
+# columns to be merged, need to have same names:
+sindex_eu <- sindex_eu %>%
+  rename(YEAR = M_YEAR)  
+
+count <- count %>%
+  rename(
+    SITE_ID   = transect_id,
+    SPECIES   = species_name,
+    YEAR      = year   # si tu columna de año se llama distinto, renómbrala aquí
+  )
+
+# calculate counts per species, site and year (now they are by week)
+count_year <- count %>%
+  group_by(SITE_ID, SPECIES, YEAR) %>%
+  summarise(COUNT = sum(count, na.rm = TRUE))
+
+# YM TO CHECK WHY cpunts_year HAS LESS ROWS THAT 
 
 # este va a ser el mejor TFG ever
