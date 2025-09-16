59b7073d1f1938bf596a94aa90340638bf63070a
ymelero <y.melero@creaf.uab.cat>
2025-09-16 12:34:40 +0200

    # code updated

--- R/0_Data_Management.R
+++ R/0_Data_Management.R
@@ -1,3 +1,5 @@
+# este va a ser el mejor TFG ever
+
 # -------------------------------------------------------------------------
 # SCRIPT TO PREPARE THE DATA: S-index (avaiable) - missing species counts / site / year
 # Author: [Papallones Urbanitas Team: YM, DGC]
@@ -19,17 +21,17 @@ library(tidyverse)
 
 ## 2.1 eBMS s-index data. per species, site, year. Calculated following Colom et al 2025
 # Give the path to the data, load them all and create an unique DB. 
-path <- "/Users/SUITCASE/Downloads/sindex_results" #YM computer
+# path <- "/Users/SUITCASE/Downloads/sindex_results" #YM computer
 
 # bind all files
-list.files(path, pattern="\\.csv$", full.names=TRUE) 
-sindex_eu <- rbindlist(lapply(list.files(path, pattern="\\.csv$", full.names=TRUE), fread))
-write.table(sindex_eu, "Data/Db_sindex_all.txt", row.names = F, sep=";")
+# list.files(path, pattern="\\.csv$", full.names=TRUE) 
+# sindex_eu <- rbindlist(lapply(list.files(path, pattern="\\.csv$", full.names=TRUE), fread))
+# write.table(sindex_eu, "Data/Db_sindex_all.txt", row.names = F, sep=";")
 # once done you can directly read the DB from your local folder:
 sindex_eu <- read.delim("Data/Db_sindex_all.txt", sep=";")
 
 ## 2.2 eBMS raw count data. To merge with s-index and detect those that failed
-count <- read.csv2("Data/ebms_count.csv", sep=",")
+count <- read.csv2("Data/ebms_uBMS_count_2023.txt", sep=";")[, 3:9]
 
 ## 2.3 merge (unir) las DBs filling those that were count but there is no info on s-index
 # columns to be merged, need to have same names:
@@ -38,27 +40,46 @@ sindex_eu <- sindex_eu %>%
 
 count <- count %>%
   rename(
-    SITE_ID   = transect_id,
-    SPECIES   = species_name,
-    YEAR      = year   # si tu columna de año se llama distinto, renómbrala aquí
+    #SITE_ID   = transect_id,
+    #SPECIES   = species_name,
+    YEAR      = year   
   )
 
-# calculate counts per species, site and year (now they are by week)
+# calculate counts per species, site and year (now they are by week) in the counts DBå
 count_year <- count %>%
   group_by(SITE_ID, SPECIES, YEAR) %>%
-  summarise(COUNT = sum(count, na.rm = TRUE))
-
-# YM TO CHECK WHY counts_year HAS LESS ROWS THAT SINDEX. dups?
+  summarise(COUNT = sum(COUNT, na.rm = TRUE))
 
 
 # Merge DBs, add colums of counts and of failed:
 sindex_eu <- sindex_eu %>%
   full_join(count_year, by = c("SITE_ID","SPECIES","YEAR")) %>%
   mutate(
-    COUNT  = replace_na(COUNT, 0),
-    FAILED = ifelse(COUNT > 0 & is.na(SINDEX), "failed", "ok")
+    SUCCESS = ifelse((is.na(SINDEX) & COUNT > 0) |
+                       (SINDEX == 0 & COUNT > 0) |
+                       is.infinite(SINDEX),
+                     "failed", "ok")
   )
 
-# YM TO CHECK: There are some sindex 0s ==> ok (Not failed) pero que tienen conteos NO zero!! ==> NEEDS TO BE CORRECTED
-plot(sindex_full$SINDEX, sindex_full$COUNT)
-# este va a ser el mejor TFG ever
+## 2.4 Check results of sindex-counts to look for potential errors 
+# YM checked: all count that == NA s-index is zero (as it should be), al sindex that are == NA , have counts (==> are those that failed)
+# TBDecided:
+ #1. Remove sindex == 0, means absence.
+ #2. Check outliers: very high counts with very low sindex and vice versa*
+plot(sindex_eu$COUNT,sindex_eu$SINDEX)
+plot(sindex_eu$COUNT,sindex_eu$SINDEX, xlim = c(0,10000), ylim=c(0,50000)) 
+ # outliers in sindex high values
+check1<- sindex_eu %>% #inf de DE, los demás casi todos BMS Vasco
+  filter(SINDEX > 10000, COUNT > 0, COUNT < 10000) %>%
+  arrange(desc(SINDEX))
+# outliers in sindex low values, high count values
+plot(sindex_eu$COUNT, sindex_eu$SINDEX, ylim = c(0,100))
+with(subset(sindex_eu, SUCCESS == "ok"),
+     plot(COUNT, SINDEX, xlim=c(0,200), ylim = c(0,100)))
+check2 <- sindex_eu %>%
+  filter(SUCCESS == "ok", SINDEX < 10, COUNT > 0, COUNT < 20) %>%
+  arrange(desc(SINDEX)) # todas las raras son failed already
+
+# Criteria to deal with outliers
+ #1. Check 1=> categorised them all as failed, and then infer from extrapolation. Do not remove => you remove species from the community. Unless the entire site / year is removed
+ #2. Check 2=> outliers are all already categorised as failure. To be exptrapolated
